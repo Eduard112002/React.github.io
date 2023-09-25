ebfcf9451547ae1f9db042195efd4dfccb7cb75a
Эдуард <123071598+Eduard112002@users.noreply.github.com>
2023-09-25 22:44:30 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,22 +1,22 @@
-import React, { Component } from 'react';
-import { createRoot } from 'react-dom/client';
-import './style.css';
+import React, { Component } from "react";
+import { createRoot } from "react-dom/client";
+import "./style.css";
 
-import NewTaskForm from './components/new-task-form';
-import TaskList from './components/task-list/task-list';
-import Footer from './components/footer/footer';
+import NewTaskForm from "./components/new-task-form";
+import TaskList from "./components/task-list/task-list";
+import Footer from "./components/footer/footer";
 
 class TodoApp extends Component {
   maxId = 100;
 
   state = {
     styleLi: [],
-    filter: 'All',
+    filter: "All",
     butEL: [
-      { text: 'All', selected: false, id: 'Al' },
-      { text: 'Active', selected: false, id: 'Ac' },
-      { text: 'Completed', selected: false, id: 'Com' },
-    ],
+      { text: "All", selected: false, id: "Al" },
+      { text: "Active", selected: false, id: "Ac" },
+      { text: "Completed", selected: false, id: "Com" }
+    ]
   };
 
   createItem = (label, min, sec) => {
@@ -27,7 +27,7 @@ class TodoApp extends Component {
       id: (this.maxId += 1),
       edit: false,
       min: min,
-      sec: sec,
+      sec: sec
     };
   };
 
@@ -38,7 +38,7 @@ class TodoApp extends Component {
       const newAll = [...styleLi.slice(0, index), ...styleLi.slice(index + 1)];
 
       return {
-        styleLi: newAll,
+        styleLi: newAll
       };
     });
   };
@@ -48,7 +48,7 @@ class TodoApp extends Component {
     const oldItem = arr[index];
     const newItem = {
       ...oldItem,
-      [propName]: !oldItem[propName],
+      [propName]: !oldItem[propName]
     };
     return [...arr.slice(0, index), newItem, ...arr.slice(index + 1)];
   }
@@ -71,20 +71,21 @@ class TodoApp extends Component {
 
   onLabelDone = (id) => {
     this.setState(({ styleLi }) => {
-      const newArray = this.toggleProperty(styleLi, id, 'done');
+      const newArrayDone = this.toggleProperty(styleLi, id, "done");
+      const newArra = this.toggleProperty(newArrayDone, id, "timeDone");
       return {
-        styleLi: newArray,
+        styleLi: newArra
       };
     });
   };
 
   conditionTodo = (text, id) => {
-    const newArr = this.conditionProperty(this.state.butEL, id, 'selected');
+    const newArr = this.conditionProperty(this.state.butEL, id, "selected");
 
     this.setState(() => {
       return {
         filter: text,
-        butEL: newArr,
+        butEL: newArr
       };
     });
   };
@@ -93,20 +94,20 @@ class TodoApp extends Component {
     const newMAs = this.state.styleLi.filter((el) => !el.done);
     this.setState(() => {
       return {
-        styleLi: newMAs,
+        styleLi: newMAs
       };
     });
   };
 
   filterTodoList = (item) => {
     switch (this.state.filter) {
-      case 'All':
+      case "All":
         return item;
 
-      case 'Active':
+      case "Active":
         return item.filter((el) => !el.done);
 
-      case 'Completed':
+      case "Completed":
         return item.filter((el) => el.done);
     }
   };
@@ -117,9 +118,57 @@ class TodoApp extends Component {
       const newArr = [...styleLi, newItem];
 
       return {
-        styleLi: newArr,
+        styleLi: newArr
       };
     });
+  };
+
+  timerStyleLi = (min, sec, id) => {
+    this.setState(({ styleLi }) => {
+      const index = styleLi.findIndex((el) => el.id === id);
+      const oldItem = styleLi[index];
+      const newItem = {
+        ...oldItem,
+        min: min,
+        sec: sec
+      };
+      const newArr = [...styleLi.slice(0, index), newItem, ...styleLi.slice(index + 1)];
+      return {
+        styleLi: newArr
+      };
+    });
+  };
+  id = null;
+  taskTimerOld = (id, timerFon) => {
+    if (timerFon) {
+      this.id = setInterval(() => {
+        const itemEl = this.state.styleLi.find((el) => el.id === id);
+        if (!itemEl) {
+          return null;
+        }
+        const min = itemEl.min;
+        const sec = itemEl.sec;
+        let newMin;
+        let newSec;
+        if (sec === 0 && min !== 0) {
+          newSec = 59;
+          newMin = min - 1;
+        } else if (min === 0 && sec === 0) {
+          clearInterval(this.id);
+          return {
+            min: 0,
+            sec: 0
+          };
+        }
+        if (sec !== 0) {
+          newSec = sec - 1;
+          newMin = min;
+        }
+        this.timerStyleLi(newMin, newSec, id);
+      }, 1000);
+    } else {
+      clearInterval(this.id);
+    }
   };
 
   editLabel = (id, label) => {
@@ -128,33 +177,35 @@ class TodoApp extends Component {
       const oldArray = styleLi[index];
       const newItem = { ...oldArray, label: label };
       return {
-        styleLi: [...styleLi.slice(0, index), newItem, ...styleLi.slice(index + 1)],
+        styleLi: [...styleLi.slice(0, index), newItem, ...styleLi.slice(index + 1)]
       };
     });
   };
 
   editClass = (id) => {
     this.setState(({ styleLi }) => {
-      const newArray = this.toggleProperty(styleLi, id, 'edit');
+      const newArray = this.toggleProperty(styleLi, id, "edit");
 
       return {
-        styleLi: newArray,
+        styleLi: newArray
       };
     });
   };
+
   render() {
-    const { styleLi, butEL, taskTime } = this.state;
+    const { styleLi, butEL } = this.state;
     const doneCount = styleLi.filter((el) => !el.done).length;
     return (
       <section className="todoapp">
         <NewTaskForm addItem={this.addItem} />
         <TaskList
           taskli={this.filterTodoList(styleLi)}
-          taskTime={taskTime}
+          timerStyleLi={this.timerStyleLi}
           onDelete={this.deleted}
           onLabelDone={this.onLabelDone}
           editClass={this.editClass}
           editLabel={this.editLabel}
+          taskTimerOld={this.taskTimerOld}
         />
         <Footer
           done={doneCount}
@@ -167,6 +218,6 @@ class TodoApp extends Component {
   }
 }
 
-const container = createRoot(document.getElementById('root'));
+const container = createRoot(document.getElementById("root"));
 
 container.render(<TodoApp />);
